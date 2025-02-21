4fb9e5f939e8cd720c64e9778577dc3b0f354963
ALLEN STANLEY <156685117+allenblade-commits@users.noreply.github.com>
2025-02-21 09:24:55 +0530

    Delete GettingStarted.c

--- GettingStarted.c
+++
@@ -1,7 +0,0 @@
-// This module outputs a constant logic HIGH (1).
-module top_module(output one);
-    
-    // Assigning the output 'one' a constant value of 1'b1 (binary 1).
-    assign one = 1'b1;
-    
-endmodule
