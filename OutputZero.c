0141f8de704676e82bbac2275898508a34961002
ALLEN STANLEY <156685117+allenblade-commits@users.noreply.github.com>
2025-02-21 09:20:11 +0530

    Create OutputZero.c

---
+++ OutputZero.c
@@ -0,0 +1,7 @@
+// This module outputs a constant logic LOW (0).
+module top_module(output zero);
+    
+    // Assigning the output 'zero' a constant value of 1'b0 (binary 0).
+    assign zero = 1'b0;
+    
+endmodule
